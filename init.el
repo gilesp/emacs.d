3b6f7c8704afec0e9613e1ef1e7e0a0d255963c5
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-06-04 16:48:31 +0100

    Configure org-mode

--- init.el
+++ init.el
@@ -9,7 +9,18 @@
 ;; Define a custom location for the file emacs uses to store customisations:
 (setq custom-file "~/.emacs.d/lisp/custom.el")
 
+;;
 ;; perform any other configuration we require
+;;
+
+;; org-mode configuration
+(require 'org)
+;; automatically use org mode for .org, .org_archive and .txt files
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+;; standard key bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; don't forget to load our custom file
 (load custom-file)
