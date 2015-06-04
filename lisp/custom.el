cbf2f3aa104283cdb21499b58d0c0e59c8e94186
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-06-04 16:48:52 +0100

    set initial buffer to TODO.org

--- lisp/custom.el
+++ lisp/custom.el
@@ -5,7 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes (quote (wheatgrass)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice "~/Documents/TODO.org")
+ '(org-agenda-files (quote ("~/Documents/TODO.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
