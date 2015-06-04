501ef6167969036997a1713584065250039c8241
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-06-04 17:04:38 +0100

    Disable auto save files

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(auto-save-default nil)
  '(custom-enabled-themes (quote (wheatgrass)))
  '(inhibit-startup-screen t)
  '(initial-buffer-choice "~/Documents/TODO.org")
