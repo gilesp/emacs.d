0744424490cf5c60f6a535246e91e5d335f4f4c5
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-09-30 13:40:15 +0100

    Enabled column number mode

--- lisp/custom.el
+++ lisp/custom.el
@@ -6,13 +6,13 @@
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(auto-save-default nil)
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (wheatgrass)))
  '(font-use-system-font t)
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
  '(mouse-wheel-progressive-speed nil)
  '(org-agenda-files (quote ("~/Documents/org")))
- '(scalable-fonts-allowed t)
  '(org-todo-keyword-faces
    (quote
     (("DONE" . success)
@@ -21,6 +21,7 @@
      ("TODO" . error))))
  '(org-todo-keywords (quote ((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE"))))
  '(pop-up-frames nil)
+ '(safe-local-variable-values (quote ((docker-image-name . "react-native"))))
  '(scalable-fonts-allowed t)
  '(scroll-step 1)
  '(show-paren-mode t))
