ca791316ad9bfe24d65ec52784f299b53d6a32ea
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-10-02 11:58:47 +0100

    Improved file identification and indentation by web-mode. Re-enabled flycheck
    Signed-off-by: Giles Paterson <giles.paterson@blackpepper.co.uk>

--- lisp/init-jsx.el
+++ lisp/init-jsx.el
@@ -8,44 +8,45 @@
 (require 'flycheck)
 (require 'web-mode)
 
-;; Activate automatically for .jsx, .android.js and .ios.js files
-(add-to-list 'auto-mode-alist '("\\.js[x]$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.android.js$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.ios.js$" . web-mode))
+;; Activate automatically for .jsx and .js files
+(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
 
-(setq web-mode-engines-alist '(("jsx" . "\\.js[x]$")
-			       ("jsx" . "\\.android.js$")
-			       ("jsx" . "\\.ios.js$")))
-
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]$")
-				     ("jsx" . "\\.android.js$")
-				     ("jsx" . "\\.ios.js$")))
-
+;; Ensure web-mode uses the jsx content type for js files
+(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
 
 ;; adjust indents for web-mode to 2 spaces
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-enable-css-colorization t)
+  (setq indent-tabs-mode nil)
   (subword-mode)
   (js2-minor-mode)
-  ;;(flycheck-mode)
+
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "jsx")
+	(let ((web-mode-enable-part-face nil))
+	  ad-do-it)
+      ad-do-it))
+
+  (flycheck-mode)
+  ;; disable jshint so we can use eslint instead
+  (setq-default flycheck-disabled-checkers
+		(append flycheck-disabled-checkers
+			'(javascript-jshint)))
+  ;; use eslint with web-mode for jsx files
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  ;; remove trailing whitespace
   (add-hook 'local-write-file-hooks
 	    (lambda()
 	      (delete-trailing-whitespace)
 	      nil))
   )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-
-;; disable jshint so we can use eslint instead
-;;(setq-default flycheck-disabled-checkers
-;;	      (append flycheck-disabled-checkers
-;;		      '(javascript-jshint)))
-
-;; use eslint with web-mode for jsx files
-;;(flycheck-add-mode 'javascript-eslint 'web-mode)
 
 
 (provide 'init-jsx)
