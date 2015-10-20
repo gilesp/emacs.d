a66010111c5585fc719786b13c120f39b7d1ed43
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-10-20 17:58:48 +0100

    Updated config

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -6,4 +6,9 @@
 ;; Bind magit status to C-x g for ease of use
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; Bind magit dispatch popup
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+(setq global-magit-file-buffer-mode t)
+
 (provide 'init-magit)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -5,9 +5,12 @@
 ;; You first need to check it out from git (git://orgmode.org/org-mode.git)
 ;; and build it with "make uncompiled"
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/org-mode/lisp"))
+
+(require 'org)
+
 ;; automatically use org mode for .org, .org_archive and .txt files
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
-(require 'org)
+
 ;; standard key bindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
@@ -23,5 +26,38 @@
 
 ;; Require html export mode
 (require 'ox-html nil t)
+
+;; Word count
+(defvar count-words-buffer
+  nil
+  "*Number of words in the buffer.")
+
+(defun wicked/update-wc ()
+  (interactive)
+  (setq count-words-buffer (number-to-string (count-words-buffer)))
+  (force-mode-line-update))
+  
+; only setup timer once
+(unless count-words-buffer
+  ;; seed count-words-paragraph
+  ;; create timer to keep count-words-paragraph updated
+  (run-with-idle-timer 1 t 'wicked/update-wc))
+
+;; add count words paragraph the mode line
+(unless (memq 'count-words-buffer global-mode-string)
+  (add-to-list 'global-mode-string "words: " t)
+  (add-to-list 'global-mode-string 'count-words-buffer t)) 
+
+;; count number of words in current paragraph
+(defun count-words-buffer ()
+  "Count the number of words in the current paragraph."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((count 0))
+      (while (not (eobp))
+	(forward-word 1)
+        (setq count (1+ count)))
+      count)))
 
 (provide 'init-org)
