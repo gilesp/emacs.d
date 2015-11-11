7517f5fa2916c09fe9d76f633f3539de1187ff80
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-11-11 16:06:46 +0000

    Speelong Correction

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;
 ;; This is the first thing emacs loads, so we can specify all our 
-;; customisations here however, it makes sens to use emacs' own 
+;; customisations here however, it makes sense to use emacs' own 
 ;; customisations functionality (which you can access with M-x customize)
 ;; for stylistic customisations and use init.el for more complex stuff such 
 ;; as keybindings or specific feature configurations
