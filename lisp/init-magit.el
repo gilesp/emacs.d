268ea1fbdcd62fb861a999d42c3eef230e41b672
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-01-14 14:03:05 +0000

    Added git-gutter to git config

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -2,6 +2,7 @@
 ;; See http://magit.vc/
 
 (require-package 'magit)
+(require-package 'git-gutter)
 
 ;; Bind magit status to C-x g for ease of use
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -10,5 +11,6 @@
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (setq global-magit-file-buffer-mode t)
+(global-git-gutter-mode t)
 
 (provide 'init-magit)
