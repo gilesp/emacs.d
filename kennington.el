9d1a3637667f61ff20dfb3c7ab845654a87bbf9b
Giles Paterson <giles@vurt.uk>
2016-01-26 20:45:48 +0000

    Modified init.el so that I can have machine specific configuration files

---
+++ kennington.el
@@ -0,0 +1,7 @@
+;; work related config
+(require 'init-dockerfile-mode)
+(require 'init-yaml-mode)
+(require 'init-slack)
+(require 'init-jsx)
+(require 'init-swift)
+;; (require 'init-java)
---
+++ tintin.el
@@ -0,0 +1,1 @@
+;; configuration specific to tintin
