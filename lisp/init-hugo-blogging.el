a2135a5377ad32319a288b6e7f71d674f04b897b
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-01-28 09:11:11 +0000

    Added org mode publishing support

--- lisp/init-hugo-blogging.el
+++ lisp/init-hugo-blogging.el
@@ -8,6 +8,23 @@
 (defun insert-timestamp ()
   "Insert the current time at point, as an ISO8601 timestamp."
   (interactive)
-  (insert (format-time-string "%Y-%n-%dT%T%z")))
+  (insert (format-time-string "%Y-%m-%dT%TZ")))
 
+;; configure markdown export
+(setq org-publish-project-alist
+      '(
+        ("org-gilesp-blog"
+         ;; Path to org files
+         :base-directory "~/Documents/vurtcouk/org-content/"
+         :base-extension "org"
+
+         ;; Path to hugo project
+         :publishing-directory "~/Documents/vurtcouk/content/"
+         :recursive t
+         :publishing-function org-md-publish-to-md
+         )
+
+        ("gilesp-blog" :components ("org-gilesp-blog"))
+        ))
+         
 (provide 'init-hugo-blogging)
