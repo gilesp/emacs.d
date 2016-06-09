3ed844e58e1d93700f24b2c6d4265060e649a1e9
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-06-09 15:44:52 +0100

    Modified linum mode face size to work better with labburn theme

--- lisp/custom.el
+++ lisp/custom.el
@@ -41,4 +41,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(linum ((t (:background "#3f3f3f" :foreground "#636363" :height 1.0)))))
