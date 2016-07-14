a16857966a60473a1d83e065f58a6488218572bf
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-14 16:50:50 +0100

    something something darkside...

--- init.el
+++ init.el
@@ -11,6 +11,13 @@
 
 ;;; Code:
 ;; make the Lisp directory available to easily load sub-files from
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; Define a custom location for the file Emacs uses to store customisations:
