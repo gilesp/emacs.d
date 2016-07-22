213876fce9f617dd71f370975d76444c0f346b4e
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 13:31:57 +0100

    Removed duplicate package-initialize

--- init.el
+++ init.el
@@ -10,11 +10,6 @@
 ;;
 
 ;;; Code:
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 
 ;; make the Lisp directory available to easily load sub-files from
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
