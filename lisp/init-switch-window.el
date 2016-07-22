f388965f8a7392ae8374b8cad00c06e660b0e075
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 17:09:52 +0100

    migrated to use-package

--- lisp/init-switch-window.el
+++ lisp/init-switch-window.el
@@ -1,8 +1,13 @@
-;;
+;;; init-switch-window --- configure better window switching
+
+;;; Commentary:
 ;; Make "C-x o" prompt for a target window when more than 2 are available
-(require-package 'switch-window)
-(require 'switch-window)
-(setq switch-window-shortcut-style 'alphabet)
-(global-set-key (kbd "C-x o") 'switch-window)
+
+;;; Code:
+(use-package switch-window
+  :config
+  (setq switch-window-shortcut-style 'alphabet)
+  :bind (("C-x o" . switch-window)))
 
 (provide 'init-switch-window)
+;;; init-switch-window.el ends here
