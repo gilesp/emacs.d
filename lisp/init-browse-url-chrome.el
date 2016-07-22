b83cb4455c33a9fd9d1f739d0895400c2891c98c
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 12:25:04 +0100

    lint fixes

--- lisp/init-browse-url-chrome.el
+++ lisp/init-browse-url-chrome.el
@@ -1,5 +1,11 @@
+;;; init-browse-url-chrome --- Use google chrome for browing urls
+
+;;; Commentary:
 ;; Use Google chrome as the default browser when clicking on links
+
+;;; Code:
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
 
 (provide 'init-browse-url-chrome)
+;;; init-browse-url-chrome.el ends here
