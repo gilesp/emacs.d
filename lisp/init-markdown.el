127772c5a9a11488a268fe6af14cafe9ede38daf
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 14:42:29 +0100

    migrated to use-package

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,16 +4,13 @@
 ;;; See http://jblevins.org/projects/markdown-mode/ for more details.
 
 ;;; Code:
-(require-package 'markdown-mode)
-
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-(autoload 'gfm-mode "gfm-mode"
-  "Major mode for editing GitHub Flavored Markdown files" t)
-(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
