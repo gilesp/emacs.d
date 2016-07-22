92d9fdb53056e2524d65157c30fd576d68daa19e
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 12:25:51 +0100

    added defvar to avoid setq warning

--- lisp/init-browse-url-chrome.el
+++ lisp/init-browse-url-chrome.el
@@ -4,6 +4,7 @@
 ;; Use Google chrome as the default browser when clicking on links
 
 ;;; Code:
+(defvar browse-url-generic-program)
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
 
