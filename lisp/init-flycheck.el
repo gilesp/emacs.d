54177497f0cfba6f6012a37a79091a22cc54cb60
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 13:43:20 +0100

    Migrated to use-package

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,3 +1,11 @@
+;;; init-flycheck --- configure flycheck globally
+;;; Commentary:
+;;; Code:
 (require-package 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(use-package flycheck
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
 (provide 'init-flycheck)
+;;; init-flycheck.el ends here
