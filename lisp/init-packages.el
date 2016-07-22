637e5c5023543183fef7f14d14afbf0c4d3d21dd
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-07-22 11:54:37 +0100

    Introduced use-package as precursor to migrating to its usage

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -4,24 +4,30 @@
 ;;; Code:
 (require 'package)
 
+(setq package-enable-at-startup nil)
+
 ;; Setup package repositores
 (add-to-list 'package-archives '("melpa" ."http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" ."http://stable.melpa.org/packages/"))
 
+(package-initialize)
+
+
 ;; Utility function to load packages when required by other config files
 (defun require-package (package)
-  "Ensure requested PACKAGE is installed."
+  "DEPRECATED - Ensure requested PACKAGE is installed."
   (if (package-installed-p package)
       t
     (progn
       (package-refresh-contents)
       (package-install package))))
 
-(setq package-enable-at-startup nil)
-(package-initialize)
+;; Bootstrap use-package
+(require-package 'use-package)
 
-(require-package 'cl-lib)
-(require 'cl-lib)
+;; Enable use of common lisp functions
+;;(require-package 'cl-lib)
+;;(require 'cl-lib)
 
 (provide 'init-packages)
 ;;; init-packages ends here
