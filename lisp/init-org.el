f631d3b4818a2c09eefeedbf44e2a1a88013a2d2
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-09-16 15:58:40 +0100

    Added org-mode hook to prevent config leaking

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -34,76 +34,84 @@
       (add-to-list 'org-modules m)))
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
-         ("C-c b" . org-iswitchb)
          ("C-c c" . org-capture))
   :config
-  (mapc 'require org-modules)
-  ;; configuration
-  (setq org-directory "~/Documents/Dropbox/org")
+  (progn
+    (mapc 'require org-modules)
+    ;; configuration
+    (setq org-directory "~/Documents/Dropbox/org")
+    
+    ;; agenda
+    (setq org-agenda-window-setup (quote current-window))
+    (setq org-agenda-files (list (expand-file-name "todo.org" org-directory)
+                                 (expand-file-name "projects/" org-directory)
+                                 (expand-file-name "projects/work/" org-directory)
+                                 (expand-file-name "projects/home/" org-directory)))
+    
+    (add-hook 'org-mode-hook 'turn-on-auto-fill)
+    
+    (setq org-todo-keyword-faces
+          (quote
+           (("DONE" . success)
+            ("STARTED" . diary)
+            ("WAITING" . warning)
+            ("TODO" . error))))
+    (setq org-todo-keywords '((sequence "TODO(t)" "PLAN(p)" "NEXT-ACTION(n)" "STARTED(s)" "WAITING(w@/!)" "DEFERRED(e)" "APPT" "|" "DONE(d!/!)" "CANCELLED(c@/!)")))
 
-  (add-hook 'org-mode-hook 'turn-on-auto-fill)
-  
-  ;; agenda
-  (setq org-agenda-window-setup (quote current-window))
-  (setq org-agenda-files (list (expand-file-name "todo.org" org-directory)))
-  
-  (setq org-todo-keyword-faces
-        (quote
-         (("DONE" . success)
-          ("STARTED" . diary)
-          ("WAITING" . warning)
-          ("TODO" . error))))
-  (setq org-todo-keywords '((sequence "TODO(t)" "PLAN(p)" "NEXT-ACTION(n)" "STARTED(s)" "WAITING(w@/!)" "DEFERRED(e)" "APPT" "|" "DONE(d!/!)" "CANCELLED(c@/!)")))
+    (setq org-capture-templates
+          '(("t" "Todo"
+             entry (file+headline "todo.org" "Tasks")
+             "* TODO [#B] %?")
+            ("j" "Journal Entry"
+             entry (file+datetree "journal.org")
+             "* %?"
+             :empty-lines 1)
+            ))
 
-  (setq org-capture-templates
-        '(("t" "Todo"
-           entry (file+headline "todo.org" "Tasks")
-           "* TODO [#B] %?")
-          ("j" "Journal Entry"
-           entry (file+datetree "journal.org")
-           "* %?"
-           :empty-lines 1)
-          ))
+    ;; actually emphasise text (e.g. show as italic instead of /italic/)
+    (setq org-hide-emphasis-markers t)
 
-  ;; actually emphasise text (e.g. show as italic instead of /italic/)
-  (setq org-hide-emphasis-markers t)
+    ;; syntax highlight code blocks
+    (setq org-src-fontify-natively t)
 
-  ;; syntax highlight code blocks
-  (setq org-src-fontify-natively t)
+    ;; replace list indicators with bullet points
+    (font-lock-add-keywords 'org-mode
+                            '(("^ +\\([-*]\\) "
+                               (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+    (org-bullets-mode 1)
 
-  ;; replace list indicators with bullet points
-  (font-lock-add-keywords 'org-mode
-                          '(("^ +\\([-*]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-  (org-bullets-mode 1)
+    ;; enable on the fly spell checking
+    (flyspell-mode 1)
 
-  ;; enable on the fly spell checking
-  (flyspell-mode 1)
+    
 
-  ;;set variable pitch font for org, but keep fixed width for code etc.
-  (variable-pitch-mode t)
-  (setq line-spacing 3)
+    (defun gp-org-mode-hook ()
+      ;;set variable pitch font for org, but keep fixed width for code etc.
+      (variable-pitch-mode t)
+      (setq line-spacing 3)
 
-  ;; disable linum
-  (linum-mode -1)
+      ;; disable linum
+      (linum-mode -1)
 
-  (defun gp-adjoin-to-list-or-symbol (element list-or-symbol)
-    (let ((list (if (not (listp list-or-symbol))
-                    (list list-or-symbol)
-                  list-or-symbol)))
-      (require 'cl-lib)
-      (cl-adjoin element list)))
+      (defun gp-adjoin-to-list-or-symbol (element list-or-symbol)
+        (let ((list (if (not (listp list-or-symbol))
+                        (list list-or-symbol)
+                      list-or-symbol)))
+          (require 'cl-lib)
+          (cl-adjoin element list)))
 
-  (mapc
-   (lambda (face)
-     (set-face-attribute
-      face nil
-      :inherit
-      (gp-adjoin-to-list-or-symbol
-       'fixed-pitch
-       (face-attribute face :inherit))))
-   (list 'org-code 'org-block 'org-block-begin-line 'org-block-end-line 'org-verbatim 'org-macro 'org-table))
-  )
+      (mapc
+       (lambda (face)
+         (set-face-attribute
+          face nil
+          :inherit
+          (gp-adjoin-to-list-or-symbol
+           'fixed-pitch
+           (face-attribute face :inherit))))
+       (list 'org-code 'org-block 'org-block-begin-line 'org-block-end-line 'org-verbatim 'org-macro 'org-table))
+      )
+    (add-hook 'org-mode-hook 'gp-org-mode-hook)
+    ))
 
 ;; TODO: Figure out where the odt schema files live so I can include them in the config
 ;; Require OpenDocument Text export mode
