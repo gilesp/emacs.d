96c752b25285b8923d071bbd37cc304c0b90e324
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-09-19 16:45:51 +0100

    Fixed config to better work with use-package

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -17,30 +17,43 @@
 (defvar org-src-fontify-natively)
 
 
+;; hook to run when org-mode is started, to turn on certain modes etc.
+(defun gp/org-mode-hook ()
+  ;;set variable pitch font for org, but keep fixed width for code etc.
+  (variable-pitch-mode t)
+
+  ;; disable linum
+  (linum-mode -1)
+
+  ;; enable on the fly spell checking
+  (flyspell-mode 1))
+
 (use-package writegood-mode
   :bind (("C-c C-g g" . writegood-grade-level)
          ("C-c C-g e" . writegood-reading-ease)))
 
+(use-package org-bullets
+ :ensure t
+ :config
+ (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 (use-package org
   :mode (("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
   :ensure org-plus-contrib
-  :init
-  (require-package 'org-bullets)
-  (let ((my-org-modules
-         '(ox-md
-           ox-gfm
-           ox-html)))
-    (dolist (m my-org-modules)
-      (add-to-list 'org-modules m)))
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c c" . org-capture))
   :config
   (progn
-    (mapc 'require org-modules)
+    (require 'ox-md)
+    (require 'ox-gfm)
+    (require 'ox-html)
+    
+    (add-hook 'org-mode-hook 'gp/org-mode-hook)
+    
     ;; configuration
     (setq org-directory "~/Documents/Dropbox/org")
-
+    (setq line-spacing 3)
     ;;
     ;; Agenda config
     ;;
@@ -145,37 +158,27 @@
     (font-lock-add-keywords 'org-mode
                             '(("^ +\\([-*]\\) "
                                (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-    (org-bullets-mode 1)
+    
 
-    (defun gp/org-mode-hook ()
-      ;;set variable pitch font for org, but keep fixed width for code etc.
-      (variable-pitch-mode t)
-      (setq line-spacing 3)
+    (defun gp/adjoin-to-list-or-symbol (element list-or-symbol)
+      (let ((list (if (not (listp list-or-symbol))
+                      (list list-or-symbol)
+                    list-or-symbol)))
+        (require 'cl-lib)
+        (cl-adjoin element list)))
 
-      ;; disable linum
-      (linum-mode -1)
-
-      ;; enable on the fly spell checking
-      (flyspell-mode 1)
-      
-      (defun gp/adjoin-to-list-or-symbol (element list-or-symbol)
-        (let ((list (if (not (listp list-or-symbol))
-                        (list list-or-symbol)
-                      list-or-symbol)))
-          (require 'cl-lib)
-          (cl-adjoin element list)))
-
-      (mapc
-       (lambda (face)
-         (set-face-attribute
-          face nil
-          :inherit
-          (gp/adjoin-to-list-or-symbol
-           'fixed-pitch
-           (face-attribute face :inherit))))
-       (list 'org-code 'org-block 'org-block-begin-line 'org-block-end-line 'org-verbatim 'org-macro 'org-table))
-      )
-    (add-hook 'org-mode-hook 'gp/org-mode-hook)
+    (mapc
+     (lambda (face)
+       (set-face-attribute
+        face nil
+        :inherit
+        (gp/adjoin-to-list-or-symbol
+         'fixed-pitch
+         (face-attribute face :inherit))))
+     (list 'org-code 'org-block 'org-block-begin-line 'org-block-end-line 'org-verbatim 'org-macro 'org-table 'org-link 'org-footnote 'org-date))
+    
+    
+    
     ))
 
 ;; TODO: Figure out where the odt schema files live so I can include them in the config
