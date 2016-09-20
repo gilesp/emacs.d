0895c9f46d8e3d4bdd8c50ee7e494886e2632c17
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-09-20 16:59:31 +0100

    Deprecated slack and mu4e config

--- kennington.el
+++ kennington.el
@@ -8,11 +8,11 @@
 ;; work related config
 (require 'init-dockerfile-mode)
 (require 'init-yaml-mode)
-(require 'init-slack)
+;;(require 'init-slack)
 (require 'init-webdev)
 (require 'init-swift)
 (require 'init-java)
-(require 'init-mu4e)
+;; (require 'init-mu4e)
 
 (provide 'kennington)
 ;;; kennington.el ends here
