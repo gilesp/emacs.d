35875f9bbcc4bc9efc144aa4779de827b4c37c39
Giles Paterson <giles.paterson@blackpepper.co.uk>
2016-11-07 12:45:53 +0000

    Commented out package-initialize inserted by emacs 25

--- init.el
+++ init.el
@@ -12,6 +12,13 @@
 ;;; Code:
 
 ;; make the Lisp directory available to easily load sub-files from
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; store any non-packaged lisp code in a local site-lisp
