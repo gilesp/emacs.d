366beeeb582409dd1b8877a0c0ec1f164fe5704d
Giles Paterson <giles.paterson@blackpepper.co.uk>
2017-02-02 12:48:49 +0000

    Updated custom.el - mostly font config

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,6 +8,9 @@
  '(auto-save-default nil)
  '(backup-directory-alist (quote (("." . "~/.saves"))))
  '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("2eccc1073f44cf1e0da01e20f3e7954cdd003ec18abfb24302f2b40cf6c1a78d" default)))
  '(default-frame-alist (quote ((width . 120) (height . 52))))
  '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote right))
@@ -18,9 +21,12 @@
     ("^\\(Enter \\)?[Pp]assphrase\\( for \\(RSA \\)?key '.*'\\)?: ?$" "^\\(Enter \\)?[Pp]assword\\( for '\\(?99:.*\\)'\\)?: ?$" "^.*'s password: ?$" "^Yubikey for .*: ?$" "^Enter PIN for '.*': ?$")))
  '(make-backup-files t)
  '(mouse-wheel-progressive-speed nil)
+ '(org-agenda-files
+   (quote
+    ("/home/giles/Documents/Dropbox/org/todo.org" "/home/giles/Documents/Dropbox/org/refile.org" "/home/giles/Documents/Dropbox/org/projects/work/brown_bags.org" "/home/giles/Documents/Dropbox/org/projects/work/hackathon.org" "/home/giles/Documents/Dropbox/org/projects/work/innovation-strategy-mindmap.org" "/home/giles/Documents/Dropbox/org/projects/work/innovation-strategy.org" "/home/giles/Documents/Dropbox/org/projects/personal/bike.org" "/home/giles/Documents/Dropbox/org/projects/personal/emacs.org" "/home/giles/Documents/Dropbox/org/projects/personal/house.org" "/home/giles/Documents/Dropbox/org/projects/personal/musical_toddler_toy.org")))
  '(package-selected-packages
    (quote
-    (rainbow-identifiers eclim color-identifiers-mode tern js2-mode yasnippet git-gutter magit neotree projectile helm ox-gfm ox-tufte company-emacs-eclim zoom-frm yaml-mode writegood-mode which-key web-mode use-package toml-mode tide switch-window swift-mode spaceline smooth-scrolling sass-mode origami org-plus-contrib org-gcal org-bullets markdown-mode labburn-theme json-mode helm-projectile helm-ls-git helm-git-grep helm-descbinds dockerfile-mode company-tern)))
+    (uniquify rainbow-identifiers eclim color-identifiers-mode tern js2-mode yasnippet git-gutter magit neotree projectile helm ox-gfm ox-tufte company-emacs-eclim zoom-frm yaml-mode writegood-mode which-key web-mode use-package toml-mode tide switch-window swift-mode spaceline smooth-scrolling sass-mode origami org-plus-contrib org-gcal org-bullets markdown-mode labburn-theme json-mode helm-projectile helm-ls-git helm-git-grep helm-descbinds dockerfile-mode company-tern)))
  '(pop-up-frames nil)
  '(safe-local-variable-values
    (quote
@@ -35,6 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dadaca" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "Hack"))))
+ '(fixed-pitch ((t (:family "Hack"))))
  '(italic ((t (:slant italic))))
  '(linum ((t (:background "#3f3f3f" :foreground "#636363" :height 0.9)))))
