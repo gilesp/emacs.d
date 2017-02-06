3831f2d323f1a596cf740bef98863501b789e512
Giles Paterson <giles.paterson@blackpepper.co.uk>
2017-02-06 13:44:05 +0000

    Added new host specific config for new laptop

---
+++ norbiton.el
@@ -0,0 +1,18 @@
+;;; norbiton.el --- Configuration specific to my work laptop
+
+;;; Commentary:
+
+;; Place config here that is only required at blackpepper
+
+;;; Code:
+;; work related config
+(require 'init-dockerfile-mode)
+(require 'init-yaml-mode)
+;;(require 'init-slack)
+(require 'init-webdev)
+(require 'init-swift)
+(require 'init-java)
+;; (require 'init-mu4e)
+
+(provide 'kennington)
+;;; norbiton.el ends here
