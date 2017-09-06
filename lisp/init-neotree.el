b5aa1c8c036cf7d57c682ee3a85ac8d4705717f5
Giles Paterson <giles.paterson@blackpepper.co.uk>
2017-09-06 17:03:21 +0100

    switched to use-package config

--- lisp/init-neotree.el
+++ lisp/init-neotree.el
@@ -1,8 +1,37 @@
+;;; init-neotree --- configure neotree
+
+;;; Commentary:
 ;; Neotree
 ;; http://www.emacswiki.org/emacs/NeoTree
 
-(require-package 'neotree)
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+;;; Code:
+(use-package  neotree
+  :bind ("<f8>" . gp/neotree-project-dir-toggle)
+  :defer
+  :config
+  (setq neo-smart-open t)
+
+  (defun gp/neotree-project-dir-toggle ()
+  "Open NeoTree using the project root, using find-file-in-project,
+or the current buffer directory."
+  (interactive)
+  (let ((project-dir
+         (ignore-errors
+           ;;; Pick one: projectile or find-file-in-project
+           (projectile-project-root)
+           ;(ffip-project-root)
+           ))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (progn
+        (neotree-show)
+        (if project-dir
+            (neotree-dir project-dir))
+        (if file-name
+            (neotree-find file-name)))))))
 
 (provide 'init-neotree)
+;;; init-neotree.el ends here
