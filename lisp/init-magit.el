e2171bc7e941d153da41881cf39d63e191594458
Giles Paterson <giles.paterson@blackpepper.co.uk>
2017-09-06 16:09:12 +0100

    switched config to use-package and enabled live updating of git-gutter

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -7,30 +7,21 @@
 
 (use-package git-gutter
   :diminish git-gutter-mode
+  :init
+  (custom-set-variables
+   '(git-gutter:update-interval 2))
+  
   :config
   (global-git-gutter-mode))
 
-(require-package 'magit)
+(use-package magit
+  :bind (("C-x g" . magit-status)
+         ("C-x M-g" . magit-dispatch-popup))
+  :config
+  (setq
+   magit-process-connection-type t
+   global-magit-file-buffer-mode t
+   git-commit-summary-max-length 120))
 
-(require 'magit)
-
-;; Bind magit status to C-x g for ease of use
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; Bind magit dispatch popup
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-
-(setq magit-process-connection-type t)
-(setq global-magit-file-buffer-mode t)
-
-(setq magit-process-password-prompt-regexps
-      '("^\\(Enter \\)?[Pp]assphrase\\( for \\(RSA \\)?key '.*'\\)?: ?$"
-        ;; match-group 99 is used to identify a host
-        "^\\(Enter \\)?[Pp]assword\\( for '\\(?99:.*\\)'\\)?: ?$"
-        "^.*'s password: ?$"
-        "^Yubikey for .*: ?$"
-        "^Enter PIN for '.*': ?$"))
-
-(setq git-commit-summary-max-length 120)
 (provide 'init-magit)
 ;;; init-magit ends here
