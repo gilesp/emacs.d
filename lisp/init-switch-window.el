07d336e8270bdeba4b9bd72a4ba898b98cb74016
Giles Paterson <giles.paterson@blackpepper.co.uk>
2018-08-20 16:35:27 +0100

    Switch window tweaks

--- lisp/init-switch-window.el
+++ lisp/init-switch-window.el
@@ -5,9 +5,11 @@
 
 ;;; Code:
 (use-package switch-window
-  :config
-  (setq switch-window-shortcut-style 'alphabet)
-  :bind (("C-x o" . switch-window)))
+  :bind (("C-x o" . switch-window)
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete)))
 
 (provide 'init-switch-window)
 ;;; init-switch-window.el ends here
