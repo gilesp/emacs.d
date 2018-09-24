decf5b110a5cc8f38d3249eec4d013585347405a
Giles Paterson <giles.paterson@blackpepper.co.uk>
2018-09-24 17:03:33 +0100

    Moved setq always ensure to inside eval-when-compile block

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -5,12 +5,10 @@
 (require 'package)
 
 (setq package-enable-at-startup nil)
-
 ;; Setup package repositores
 (add-to-list 'package-archives '("melpa" ."https://melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("melpa-stable" ."http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-
 (package-initialize)
 
 ;; Utility function to load packages when required by other config files
@@ -24,9 +22,10 @@
 
 ;; Bootstrap use-package
 (require-package 'use-package)
-(eval-when-compile (require 'use-package))
-;; makesure use-package auto install packages
-(setq use-package-always-ensure t)
+(eval-when-compile
+  (require 'use-package)
+  ;; makesure use-package auto install packages
+  (setq use-package-always-ensure t))
 
 ;; Checks for package update
 (use-package auto-package-update
