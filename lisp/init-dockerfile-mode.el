30d162f0b3f7e642259a6e4f83322ab849dbfb9d
Giles Paterson <giles.paterson@blackpepper.co.uk>
2019-04-09 09:52:22 +0100

    Updated docker mode config

--- lisp/init-dockerfile-mode.el
+++ lisp/init-dockerfile-mode.el
@@ -4,7 +4,10 @@
 
 ;;; Code:
 (use-package dockerfile-mode
-  :mode "Dockerfile\\'")
+  :mode "Dockerfile\\'"
+  :config
+  (setq tab-width 4)
+  (setq tab-stop-list (number-sequence 4 120 4)))
 
 (provide 'init-dockerfile-mode)
 ;;; init-dockerfile-mode.el ends here
